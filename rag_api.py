d4a9af6d4609f2e01749c4a48f247ed16222e79e
HaR-S-H <h8551283@gmail.com>
2025-02-28 13:59:58 +0530

    change port

--- rag_api.py
+++ rag_api.py
@@ -18,5 +18,6 @@ def get_rag_response(request: QueryRequest):
     return {"response": response}
 
 if __name__ == "__main__":
-    port=int(os.getenv("PORT"))
+    port=int(os.getenv("PORT",8000))
+    print(f"Starting server on port {port}") 
     uvicorn.run(app, host="0.0.0.0", port=port)
